667f26dbcbc58438b9b33e43db8ff045d947e141
Nurislom <nurislomx72@gmail.com>
2024-03-13 19:58:56 +0500

    added EagleEye simple project #16

--- EagleEye/LicensingService/src/main/java/org/khasanof/licensingservice/License.java
+++ EagleEye/LicensingService/src/main/java/org/khasanof/licensingservice/License.java
@@ -1,14 +1,23 @@
 package org.khasanof.licensingservice;
 
+import lombok.*;
+
 /**
  * @author Nurislom
  * @see org.khasanof.licensingservice
  * @since 3/3/2024 12:52 PM
  */
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class License {
 
+    private String id;
     private String organizationId;
     private String productName;
-    private String productType;
+    private String licenseType;
 
 }
--- EagleEye/LicensingService/src/main/java/org/khasanof/licensingservice/LicensingServiceApplication.java
+++ EagleEye/LicensingService/src/main/java/org/khasanof/licensingservice/LicensingServiceApplication.java
@@ -2,6 +2,11 @@ package org.khasanof.licensingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LicensingServiceApplication {
@@ -10,4 +15,19 @@ public class LicensingServiceApplication {
         SpringApplication.run(LicensingServiceApplication.class, args);
     }
 
+}
+
+@RestController
+@RequestMapping(value = "/organizations")
+class LicenseController {
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense(@PathVariable String licenseId) {
+        return ResponseEntity.ok(License.builder()
+                        .id(licenseId)
+                        .organizationId("TestOrg")
+                        .productName("Teleco")
+                        .licenseType("Seat")
+                .build());
+    }
 }
